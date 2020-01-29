3c62efe0f0931823724d1dfe0a5f6c31d49a9ad7
Ferdinand Ratajczak <fe.ratajczak@gmail.com>
2020-01-29 14:13:01 +0100

    Don't free ft_printf format string

--- libft/src/ft_printf/pf_utils/pf_del.c
+++ libft/src/ft_printf/pf_utils/pf_del.c
@@ -6,7 +6,7 @@
 /*   By: nbousset <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/11 20:07:44 by nbousset          #+#    #+#             */
-/*   Updated: 2019/04/15 01:08:47 by nbousset         ###   ########.fr       */
+/*   Updated: 2020/01/29 13:53:34 by fratajcz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void		pf_del(t_printf *pf)
 {
 	ft_bzero(pf, sizeof(t_printf));
 	ft_memdel((void **)&pf->str);
-	ft_memdel((void **)&pf->format);
 	pf->str = NULL;
 	pf->format_start = NULL;
 	pf->format = NULL;
