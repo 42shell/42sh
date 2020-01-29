a0010f2d6266ec8cc13a9a512faf7d780649ca75
Ferdinand Ratajczak <fe.ratajczak@gmail.com>
2020-01-29 17:49:29 +0100

    remove fflush

--- src/shell/path_utils.c
+++ src/shell/path_utils.c
@@ -6,7 +6,7 @@
 /*   By: fratajcz <fratajcz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/15 10:28:53 by fratajcz          #+#    #+#             */
-/*   Updated: 2020/01/22 14:48:14 by fratajcz         ###   ########.fr       */
+/*   Updated: 2020/01/29 17:49:19 by fratajcz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -113,7 +113,6 @@ char		*get_executable_path(char *command, t_env *env)
 	if ((path == NULL || path[i] == NULL))
 	{
 		ft_dprintf(2, "21sh: %s: command not found\n", command);
-		fflush(stdout);
 		g_last_exit_st = 127;
 		ret = NULL;
 	}
