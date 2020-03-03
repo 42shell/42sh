4a8cffea0ca81fda2ab699a14b492b02a47caf9b
Nicolas BOUSSETTA <nbousset@e2r4p14.42.fr>
2020-03-03 20:12:40 +0100

    refactor exec/redir.c

--- src/exec/redir_error.c
+++ src/exec/redir_error.c
@@ -1,34 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   argv.c                                             :+:      :+:    :+:   */
+/*   redir_error.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fratajcz <fratajcz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/12/15 09:08:47 by fratajcz          #+#    #+#             */
-/*   Updated: 2020/02/14 17:52:18 by fratajcz         ###   ########.fr       */
+/*   Created: 2019/12/15 09:52:31 by fratajcz          #+#    #+#             */
+/*   Updated: 2020/02/07 00:36:28 by fratajcz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-char		**get_argv(t_command *command)
+int		redir_error(int code)
 {
-	char	**argv;
-	int		argc;
-	int		i;
-
-	i = 0;
-	argc = 0;
-	if (!command->argv)
-		return (NULL);
-	while (command->argv[argc])
-		argc++;
-	argv = ft_xmalloc(sizeof(char *) * (argc + 1));
-	while (i < argc)
-	{
-		argv[i] = ft_strdup(command->argv[i]->value->str);
-		i++;
-	}
-	return (argv);
+	if (code == ERROR_REDIR_OPEN)
+		ft_dprintf(2, "42sh: Could not open file\n");
+	else if (code == ERROR_REDIR_BAD_FD)
+		ft_dprintf(2, "42sh: Bad file descriptor\n");
+	return (code);
 }
