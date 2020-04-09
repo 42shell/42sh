49847715eb4c4cdc7f2db4144f2fcf685795f2c8
Ferdinand Ratajczak <fe.ratajczak@gmail.com>
2020-04-09 23:42:19 +0200

    Fix format job for new command struct

--- src/execution/job_print.c
+++ src/execution/job_print.c
@@ -6,54 +6,58 @@
 /*   By: fratajcz <fratajcz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/15 09:08:47 by fratajcz          #+#    #+#             */
-/*   Updated: 2020/04/09 17:30:57 by fratajcz         ###   ########.fr       */
+/*   Updated: 2020/04/09 23:49:32 by fratajcz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-static void	format_command(t_command *command, t_dstr *buf)
+static void	format_simple_command(t_simple_cmd *command, t_dstr *buf)
 {
-	//t_command	*command;
-	//t_token		*word;
-	//t_redir		*redir;
+	char		*arg;
+	t_redir		*redir;
 
-	//command = (t_command *)node->data; 
-	//word = command->words;
-	//redir = command->redirs;
-	//while (word)
-	//{
-	//	ft_dstr_append(buf, word->value->str);
-	//	if ((word = word->next) || redir)
-	//		ft_dstr_append(buf, " ");
-	//}
-	//while (redir)
-	//{
-	//	ft_dstr_append(buf, redir->left_op->value->str);
-	//	ft_dstr_append(buf, redir->operator->value->str);
-	//	ft_dstr_append(buf, redir->right_op->value->str);
-	//	if ((redir = redir->next))
-	//		ft_dstr_append(buf, " ");
-	//}
-	
+	arg = command->argv->array[0];
+	redir = command->redirs;
+	while (arg)
+	{
+		ft_dstr_append(buf, arg);
+		if ((arg = arg + 1) || redir)
+			ft_dstr_append(buf, " ");
+	}
+	while (redir)
+	{
+		ft_dstr_append(buf, redir->left_op->value->str);
+		ft_dstr_append(buf, redir->operator->value->str);
+		ft_dstr_append(buf, redir->right_op->value->str);
+		if ((redir = redir->next))
+			ft_dstr_append(buf, " ");
+	}
 }
 
 t_dstr		*format_job(t_command *command, t_dstr *buf)
 {
-//	if (!command)
-//		return (NULL);
-//	if (!buf)
-//		buf = ft_dstr_new(36);
-//	format_job(command->left, buf);
-//	if (node->type == NODE_AND)
-//		ft_dstr_append(buf, " && ");
-//	else if (node->type == NODE_OR)
-//		ft_dstr_append(buf, " || ");
-//	else if (node->type == NODE_PIPE)
-//		ft_dstr_append(buf, " | ");
-//	else if (node->type == NODE_SMPL_CMD)
-//		format_command(node, buf);
-//	format_job(node->right, buf);
+	enum e_token_type connector;
+
+	if (!command)
+		return (NULL);
+	if (!buf)
+		buf = ft_dstr_new(36);
+	if (command->type == CONNECTION)
+	{
+		format_job(command->value.connection->left, buf);
+		connector = command->value.connection->connector;
+		if (connector == AND_IF)
+			ft_dstr_append(buf, " && ");
+		else if (connector == AND_IF)
+			ft_dstr_append(buf, " || ");
+		else if (connector == PIPE)
+			ft_dstr_append(buf, " | ");
+		format_job(command->value.connection->right, buf);
+	}
+	else if (command->type == SIMPLE)
+		format_simple_command(command->value.simple, buf);
+	//subshell
 	return (buf);
 }
 
