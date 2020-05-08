1f5575dcc426d984f9c4867ec2fcac0464d72d35
Ferdinand Ratajczak <fe.ratajczak@gmail.com>
2020-05-08 18:51:04 +0200

    Norm libft

--- libft/src/readline/readline.c
+++ libft/src/readline/readline.c
@@ -6,45 +6,11 @@
 /*   By: fratajcz <fratajcz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/21 20:08:08 by fratajcz          #+#    #+#             */
-/*   Updated: 2020/04/02 00:43:54 by fratajcz         ###   ########.fr       */
+/*   Updated: 2020/05/08 18:45:44 by fratajcz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "readline.h"
-
-int			rl_del(void)
-{
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &g_rl_oldterm);
-	if (g_rl_hist.head)
-	{
-		while (g_rl_hist.head->next)
-			ft_list_del(g_rl_hist.head->next);
-		ft_list_del(g_rl_hist.head);
-	}
-	ft_dstr_del(&g_rl_line.backup);
-	ft_dstr_del(&g_rl_line.clipboard);
-	ft_dstr_del(&g_rl_line.dstr);
-	ft_bzero(&g_rl_hist, sizeof(g_rl_hist));
-	ft_bzero(&g_rl_line, sizeof(g_rl_line));
-	ft_bzero(g_rl_keymap, sizeof(g_rl_keymap));
-	ft_bzero(&g_rl_key, sizeof(g_rl_key));
-	ft_bzero(g_rl_caps, sizeof(g_rl_caps));
-	ft_bzero(g_rl_keys, sizeof(g_rl_keys));
-	ft_bzero(&g_rl_term, sizeof(g_rl_term));
-	ft_bzero(&g_rl_oldterm, sizeof(g_rl_oldterm));
-	g_rl_is_init = false;
-	g_rl_retain_nl = false;
-	g_rl_line_limit = false;
-	g_rl_prompt_cr = false;
-	g_rl_hist_doubl = false;
-	g_rl_prompt = NULL;
-	g_rl_line_size_max = 0;
-	g_rl_oldkey = 0;
-	g_rl_sizex = 0;
-	g_rl_sizey = 0;
-	g_rl_posx = 0;
-	return (0);
-}
 
 static char	*rl_return(int ret)
 {
@@ -57,8 +23,8 @@ static char	*rl_return(int ret)
 		if (!g_rl_error)
 			g_rl_error = RL_ALLOC_ERROR;
 		g_rl_last_ret = g_rl_error;
-		rl_del();
-		return (NULL);
+		ft_putstr_fd("readline: fatal error", 2);
+		exit(1);
 	}
 	if (ret != RL_ENTER)
 		g_rl_last_ret = ret;
--- libft/src/struct/list/ft_list_add.c
+++ libft/src/struct/list/ft_list_add.c
@@ -6,7 +6,7 @@
 /*   By: fratajcz <fratajcz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/08 17:11:09 by fratajcz          #+#    #+#             */
-/*   Updated: 2019/11/29 00:15:24 by fratajcz         ###   ########.fr       */
+/*   Updated: 2020/05/08 18:43:57 by fratajcz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 ** add a new entry after the specified list head
 */
 
-void	ft_list_add(void *data, t_list_head *head) //<->
+void	ft_list_add(void *data, t_list_head *head)
 {
 	t_list_head *new;
 
