e8f74cd778d492059bbc6103fad0dc9002e61ab0
Ferdinand Ratajczak <fe.ratajczak@gmail.com>
2020-06-16 18:20:49 +0200

    Don't print expand error in redir_error

--- src/execution/redirect_error.c
+++ src/execution/redirect_error.c
@@ -6,7 +6,7 @@
 /*   By: fratajcz <fratajcz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/15 09:52:31 by fratajcz          #+#    #+#             */
-/*   Updated: 2020/05/28 19:58:29 by fratajcz         ###   ########.fr       */
+/*   Updated: 2020/06/16 18:15:56 by fratajcz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 
 extern char	*g_expand_error_token;
 
+/*
+** if code == ERROR_REDIR_EXPAND, the error message is already printed in expand
+*/
+
 int		redir_error(int code)
 {
 	if (code == ERROR_REDIR_OPEN)
 		ft_dprintf(2, "42sh: Could not open file\n");
 	else if (code == ERROR_REDIR_BAD_FD)
 		ft_dprintf(2, "42sh: Bad file descriptor\n");
-	else if (code == ERROR_REDIR_EXPAND)
-		ft_dprintf(2, "42sh: %s: bad substitution\n", g_expand_error_token);
 	return (code);
 }
