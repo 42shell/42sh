c75062bcf9a7dbf9fe57b0891b86095b097fa9bb
nbousset <nbousset@student.42.fr>
2020-06-17 10:58:16 +0200

    change tputs padding from 20 to 1

--- libft/src/readline/term_cursor.c
+++ libft/src/readline/term_cursor.c
@@ -16,10 +16,10 @@ void	movcright(void)
 {
 	if (g_rl_posx == g_rl_sizex - 1)
 	{
-		tputs(tgoto(g_rl_caps[C_CH], 0, 0), 20, ft_putc);
+		tputs(tgoto(g_rl_caps[C_CH], 0, 0), 1, ft_putc);
 		if (g_rl_posy < g_rl_sizey - 1)
 		{
-			tputs(g_rl_caps[C_SF], 20, ft_putc);
+			tputs(g_rl_caps[C_SF], 1, ft_putc);
 			g_rl_posy++;
 		}
 		g_rl_posx = 0;
@@ -38,14 +38,14 @@ void	movcleft(void)
 		tputs(tgoto(g_rl_caps[C_CH], 0, g_rl_sizex - 1), 20, ft_putc);
 		if (g_rl_posy > 0)
 		{
-			tputs(g_rl_caps[C_SR], 20, ft_putc);
+			tputs(g_rl_caps[C_SR], 1, ft_putc);
 			g_rl_posy--;
 		}
 		g_rl_posx = g_rl_sizex - 1;
 	}
 	else
 	{
-		tputs(g_rl_caps[C_LE], 20, ft_putc);
+		tputs(g_rl_caps[C_LE], 1, ft_putc);
 		g_rl_posx--;
 	}
 }
