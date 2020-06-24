481de005e75594e40577fba47f79ed44176ffcfe
Ferdinand Ratajczak <fe.ratajczak@gmail.com>
2020-06-24 17:25:32 +0200

    Fix norminette test on travis

--- tests/norminette.sh
+++ tests/norminette.sh
@@ -3,13 +3,21 @@
 git clone https://github.com/42Paris/norminette /tmp/norminette
 (cd /tmp/norminette; bundle)
 
-if [ $(git rev-parse --abbrev-ref HEAD) == master ]; then
+if [ -z "$TRAVIS_BRANCH" ]; then
+	TRAVIS_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+fi
+
+if [ "$TRAVIS_BRANCH" == master ]; then
 	FILES=$(git diff --name-only HEAD HEAD~ src include libft/src libft/inc)
 else
-	FILES=$(git diff --name-only master -- src include libft/src libft/inc)
+	git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+	git fetch origin master
+	FILES=$(git diff --name-only origin/master -- src include libft/src libft/inc)
 fi
-if [ $FILES -z ]; then
+
+if [ -z "$FILES" ]; then
+	echo "No .c or .h changed ✔️"
 	exit 0
 fi
 
-! /tmp/norminette/norminette.rb "$FILES" | grep -B 1 "Error"
+! /tmp/norminette/norminette.rb "$FILES" | grep -B 1 "Error" && echo "No norm error ✔️"
