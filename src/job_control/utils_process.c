062a4f88735163487b181c7a4c86e63bf331806e
nbousset <nbousset@student.42.fr>
2020-07-10 15:41:35 +0200

    remove useless function

--- src/job_control/utils_process.c
+++ src/job_control/utils_process.c
@@ -12,33 +12,6 @@
 
 #include "shell.h"
 
-t_process			*process_list_dup(t_process *process_list)
-{
-	t_process	*dup_list;
-	t_process	*dup;
-	t_process	*prev;
-
-	prev = NULL;
-	dup_list = NULL;
-	while (process_list)
-	{
-		dup = ft_xmalloc(sizeof(t_process));
-		*dup = *process_list;
-		if (!dup_list)
-		{
-			dup_list = dup;
-			prev = dup_list;
-		}
-		else
-		{
-			prev->next = dup;
-			prev = prev->next;
-		}
-		process_list = process_list->next;
-	}
-	return (dup_list);
-}
-
 static t_process	*get_process_in(t_job *list, pid_t pid)
 {
 	t_job		*job;
