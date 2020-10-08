0c838c8e07cc8272704cb1463c9dde7e6299df84
nbousset <nbousset@student.42.fr>
2020-10-08 19:04:14 +0200

    remove trash

--- tests/test.sh
+++ tests/test.sh
@@ -91,15 +91,6 @@ do
 	cp "$DIR/live_tests/$test_name.right" "$DIR/$test_name.bash"
 done
 
-##################################################
-for file in "$DIR/live_tests/job_control/"*.timing
-do
-	test_name=$(basename "$file" | cut -d '.' -f 1)
-	scriptlive -T "$DIR/live_tests/job_control/$test_name".timing --log-in "$DIR/live_tests/job_control/$test_name.stdin" --maxdelay 0.03 -c "./42sh > $DIR/$test_name.42sh"
-	cp "$DIR/live_tests/job_control/$test_name.right" "$DIR/$test_name.bash"
-done
-##################################################
-
 rm -rf testing_autocomplete
 
 for file in "$DIR/live_tests/job_control/"*.timing
