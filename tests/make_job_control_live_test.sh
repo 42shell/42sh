cb6b2cdb2d108de2c46e8873e7b927da92b80208
Nicolas BOUSSETTA <nbousset@e1r11p14.42.fr>
2020-10-08 17:57:34 +0200

    shit happens

--- tests/make_job_control_live_test.sh
+++ tests/make_job_control_live_test.sh
@@ -15,7 +15,7 @@ echo "Recording test \"$1\"..."
 
 OLD_TERMWIDTH=$(tput cols)
 stty cols 80 #force all tests to have 80 columns for consistency
-script -T "$DIR/live_tests/job_control/$1.timing" --log-in "$DIR/live_tests/job_control/$1.stdin" -c ./42sh > "$DIR/live_tests/job_control/$1.right"
+script -T "$DIR/live_tests/job_control/$1.timing" --log-in "$DIR/live_tests/job_control/$1.stdin" -c "./42sh > \"$DIR/live_tests/job_control/$1.right\""
 
 read -p "Keep test? [y/n] " -n 1 -r
 echo
@@ -27,7 +27,7 @@ fi
 
 echo "Checking reproducibility with --maxdelay 0.02..."
 
-scriptlive -T "$DIR/live_tests/job_control/$1.timing" --log-in "$DIR/live_tests/job_control/$1.stdin" --maxdelay 0.02 -c ./42sh > "$DIR/live_tests/job_control/$1.log"
+scriptlive -T "$DIR/live_tests/job_control/$1.timing" --log-in "$DIR/live_tests/job_control/$1.stdin" --maxdelay 0.02 -c "./42sh > \"$DIR/live_tests/job_control/$1.log\""
 
 diff "$DIR/live_tests/job_control/$1.log" "$DIR/live_tests/$1.right" > "$DIR/live_tests/job_control/$1.diff_log"
 if [[ $? != "0" ]]; then
