b95416c623f251e1ad5efe8586099c426991aa36
nbousset <nbousset@student.42.fr>
2020-10-12 18:18:34 +0200

    redo all job control tests with sleep .2 and stderr redirected to .right

--- tests/test.sh
+++ tests/test.sh
@@ -96,7 +96,7 @@ rm -rf testing_autocomplete
 for file in "$DIR/live_tests/job_control/"*.timing
 do
 	test_name=$(basename "$file" | cut -d '.' -f 1)
-	scriptlive -T "$DIR/live_tests/job_control/$test_name".timing --log-in "$DIR/live_tests/job_control/$test_name.stdin" --maxdelay 0.03 -c "./42sh > $DIR/$test_name.42sh" >/dev/null
+	scriptlive -T "$DIR/live_tests/job_control/$test_name".timing --log-in "$DIR/live_tests/job_control/$test_name.stdin" --maxdelay 0.03 -c "./42sh > $DIR/$test_name.42sh 2>&1" >/dev/null
 	cp "$DIR/live_tests/job_control/$test_name.right" "$DIR/$test_name.bash"
 done
 
