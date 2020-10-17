649da07fbe0fffec87fd1a373965f2657c88c83e
Nicolas BOUSSETTA <nbousset@e2r12p12.42.fr>
2020-10-17 16:35:15 +0200

    fix 2nd leak

--- src/parser/new_del.c
+++ src/parser/new_del.c
@@ -30,10 +30,10 @@ void		redir_del(t_redir **redir)
 	if (!redir || !*redir)
 		return ;
 	redir_del(&(*redir)->next);
-	token_del(&(*redir)->left_op);
-	token_del(&(*redir)->operator);
-	token_del(&(*redir)->right_op);
-	token_del(&(*redir)->heredoc);
+	token_list_del(&(*redir)->left_op);
+	token_list_del(&(*redir)->operator);
+	token_list_del(&(*redir)->right_op);
+	token_list_del(&(*redir)->heredoc);
 	ft_memdel((void **)redir);
 }
 
