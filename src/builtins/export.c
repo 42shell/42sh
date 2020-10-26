2ed89116da37c479f54379f561fa3b68023021ff
Ferdinand Ratajczak <fe.ratajczak@gmail.com>
2020-10-26 03:38:23 +0100

    Add export -p

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -20,15 +20,67 @@ static void	export_one_var(char *name, char *value)
 		set_var(name, value, V_EXPORT);
 }
 
+char		*get_quoted_string(const char *str)
+{
+	t_dstr	*quoted;
+	char	*ret;
+
+	quoted = ft_dstr_new(32);
+	ft_dstr_add(quoted, '"');
+	while (*str)
+	{
+		if (*str == '"' || *str == '\\' || *str == '$' || *str == '`')
+			ft_dstr_add(quoted, '\\');
+		ft_dstr_add(quoted, *str);
+		str++;
+	}
+	ft_dstr_add(quoted, '"');
+	ret = quoted->str;
+	free(quoted);
+	return (ret);
+}
+
+static void	add_var_to_arr(const char *key, void *value, void *obj)
+{
+	t_array			*array;
+	t_var			*var;
+	char			*quoted;
+
+	var = value;
+	if (var->attributes & V_HIDDEN || !(var->attributes & V_EXPORT))
+		return ;
+	array = obj;
+	quoted = get_quoted_string(var->value);
+	array_append(array, ft_strjoin_triple(key, "=", quoted));
+	free(quoted);
+}
+
+int			export_p(void)
+{
+	t_array *vars;
+	size_t	i;
+
+	vars = array_new(32);
+	ht_enum(g_shell.vars, add_var_to_arr, vars);
+	sort_matches((char **)vars->array, vars->size);
+	i = 0;
+	while (i < vars->size)
+		ft_printf("export %s\n", vars->array[i++]);
+	array_destroy(vars);
+	return (0);
+}
+
 int			builtin_export(char **argv, __attribute__((unused)) t_array *env)
 {
 	int		i;
 	int		ret;
 	char	*value;
 
-	i = 1;
+	i = 0;
 	ret = 0;
-	while (argv[i])
+	if (argv[1] && ft_strequ(argv[1], "-p"))
+		return (export_p());
+	while (argv[++i])
 	{
 		value = NULL;
 		if (argv[i][0] != '=' && (value = ft_strchr(argv[i], '=')))
@@ -41,9 +93,8 @@ int			builtin_export(char **argv, __attribute__((unused)) t_array *env)
 		}
 		else
 			export_one_var(argv[i], value);
-		i++;
 	}
 	if (i == 1)
-		ft_dprintf(2, "usage: export [name[=value]]\n");
+		ft_dprintf(2, "usage: export [name[=value]] or export -p\n");
 	return (ret);
 }
